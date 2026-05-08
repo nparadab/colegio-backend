2553b8d69665ea41f90b090dce3474e1ee6c4b10
nparadab <n.paradab@duocuc.cl>
2026-05-08 11:17:24 -0400

    Primer commit: backend multimódulo con módulos hijos

---
+++ microservicio_academico/src/main/java/AcademicoApplication.java
@@ -0,0 +1,11 @@
+package com.colegio.academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicoApplication.class, args);
+    }
+}
---
+++ microservicio_comunicaciones/src/main/java/ComunicacionesApplication.java
@@ -0,0 +1,11 @@
+package com.colegio.comunicaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunicacionesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComunicacionesApplication.class, args);
+    }
+}
